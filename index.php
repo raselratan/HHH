d9c7861f7ea95b10ebc1111eb8005fa61b15cb9e
raselratan <rasel.m.ratan@gmail.com>
2018-05-03 15:15:26 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<title>Home Management System</title>	
+		<link href="user/files/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<link href="user/files/css/style.css" rel="stylesheet" id="bootstrap-css">
+		<script src="user/files/js/jquery-1.11.1.min.js"></script>		
+		<script src="user/files/js/bootstrap.min.js"></script>
+		<!------ Include the above in your HEAD tag ---------->	
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="col-md-3"></div>
+			<div class="row col-md-6" style="margin-top:150px; max-width:500px">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Select Your Category
+					</div>
+					<div class="panel-body">
+						<div class="" style="margin-left:150px">
+							<a href="user" role="button" class="btn btn-primary">User</a>
+							<a href="police" role="button" class="btn btn-primary">Police</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3"></div>			
+		</div>	
+	</body>
+</html>	
